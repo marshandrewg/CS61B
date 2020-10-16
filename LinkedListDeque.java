348f4fd5bfd3facdec9f12dde70841983dcdf0fd
Andrew Marsh <marshandrewg@gmail.com>
2020-10-16 17:04:31 -0400

    Added node inner class

--- LinkedListDeque.java
+++ LinkedListDeque.java
@@ -1,6 +1,18 @@
 /** Performs some basic linked list tests. */
-public class LinkedListDequeTest {
-    public LinkedListDeque();
+public class LinkedListDeque<T> {
+    private class Node {
+        public T item;
+        public Node next;
+
+        public Node(T i, Node n) {
+            item = i;
+            next = n;
+        }
+
+    }
+    
+    public LinkedListDeque() {
+    }
     public void addFirst(T item);
     public void addLast(T item);
     public boolean isEmpty();
