4e2938a046726aba6454c0ee37427b98a647ad38
Andrew Marsh <marshandrewg@gmail.com>
2020-10-16 16:23:40 -0400

    Added skeleltons for LinkedList files

---
+++ LinkedListDeque.java
@@ -0,0 +1,13 @@
+/** Performs some basic linked list tests. */
+public class LinkedListDequeTest {
+    public LinkedListDeque();
+    public void addFirst(T item);
+    public void addLast(T item);
+    public boolean isEmpty();
+    public int size();
+    public void printDeque();
+    public T removeFirst();
+    public T removeLast();
+    public T get(int index);
+    public T getRecursive(int index);
+}
