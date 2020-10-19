dc61706f3652e0498c3d19cd935b09419a829733
Andrew Marsh <agmarsh@ncsu.edu>
2020-10-19 17:41:32 -0400

    Implemented methods addFirst, addLast, contructor

--- LinkedListDeque.java
+++ LinkedListDeque.java
@@ -3,23 +3,71 @@ public class LinkedListDeque<T> {
     private class Node {
         public T item;
         public Node next;
+        public Node prev;
 
-        public Node(T i, Node n) {
+        // Run as public vs private 
+        public Node(T i, Node n, Node p) {
             item = i;
             next = n;
+            prev = p;
         }
 
     }
     
+    Node head;
+    Node tail;
+    int size;
+
     public LinkedListDeque() {
+        head = null;
+        tail = null;
+        size = 0;
     }
-    public void addFirst(T item);
-    public void addLast(T item);
+
+    public void addFirst(T item){
+        head = new Node(item, null, null);
+        size += 1;
+    }
+
+    public void addLast(T item){
+        size += 1;
+        tail.next = new Node(item, null, tail);
+
+        // head                 tail
+        // (1) -> (2) -> (3) -> (4) -> (5)
+        
+        
+        // head                        tail
+        // (1) -> (2) -> (3) -> (4) -> (5) -> (1) ....
+        
+        tail = tail.next;
+    }
+
     public boolean isEmpty();
     public int size();
     public void printDeque();
     public T removeFirst();
-    public T removeLast();
+    public T removeLast() {
+        // head                         tail
+        // (1) -> (2) -> (3) -> (4) -> (5)
+        
+        
+        tail = tail.prev;
+        size -= 1;        
+
+        // head                 tail
+        // (1) -> (2) -> (3) -> (4) //-> (5)//
+    }
     public T get(int index);
     public T getRecursive(int index);
 }
+
+// LinkedListDeque = new LinkedListDeque("Bulbasaur")
+LinkedListDeque.jackShit()
+sizeOfList = size(list)
+printedList = print(list)
+LinkedListDeque = new LinkedListDeque("Ivysaur")
+
+// Bulbasaur
+// Ivysaur
+// Venosaur
