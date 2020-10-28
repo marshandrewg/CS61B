d2d6cdf4dcfd826f76f83f93753b4625e0cd01b4
Andrew Marsh <agmarsh@ncsu.edu>
2020-10-28 16:26:32 -0400

    Style changes and new test

--- LinkedListDeque.java
+++ LinkedListDeque.java
@@ -2,142 +2,141 @@
 
 /** Performs some basic linked list tests. */
 public class LinkedListDeque<T> {
-    private class Node {
-        public T item;
-        public Node next;
-        public Node prev;
+  private class Node {
+    public T item;
+    public Node next;
+    public Node prev;
 
-        // Run as public vs private 
-        /**
-         * Creates a node object
-         * @param i item to add to node
-         * @param n next Node
-         * @param p previous node
-         * @return Node object
-         */
-        public Node(T i, Node n, Node p) {
-            item = i;
-            next = n;
-            prev = p;
-        }
+    // Run as public vs private 
+    /**
+     * Creates a node object
+     * @param i item to add to node
+     * @param n next Node
+     * @param p previous node
+     * @return Node object
+     */
+    public Node(T i, Node n, Node p) {
+      item = i;
+      next = n;
+      prev = p;
+    }
+  }
+  
+  Node head;
+  Node tail;
+  int size;
+
+  public LinkedListDeque() {
+    head = null;
+    tail = null;
+    size = 0;
+  }
+
+  public void addFirst(T item){
+    head = new Node(item, null, null);
+    size += 1;
+  }
+
+  public void addLast(T item){
+    tail.next = new Node(item, null, tail);
+
+    // head         tail
+    // (1) -> (2) -> (3) -> (4) -> (5)
+    
+    
+    // head            tail
+    // (1) -> (2) -> (3) -> (4) -> (5) -> (1) ....
+    
+    tail = tail.next;
+    size += 1;
+  }
+
+  public boolean isEmpty() {
+    if (head != null){
+      return true;
+    }
+    return false;
+  }
+  
+  public int size() {
+    return size;
+  }
+  public void printDeque(){
+    // head             tail
+    // (1) <-> (2) <-> (3) <-> (4) -> (5)
+    Node s = head;
+    while (s.next != null) {
+      System.out.println(s.item);
+      s = s.next;
+    }
+  }
+
+  public T removeFirst(){
+    // head      
+    // (1)
+    //  <-> (2) <-> (3) <-> (4) -> (5)
+    T save = head.item;
+    head = head.next;
+    //    head         tail
+    //   <- (2) -> (3) -> (4) -> (5)
+    if(head != null){
+      head.prev = null;
+    }
+
+    size -= 1;
+    return save;
+  }
+  public T removeLast() {
+    // head             tail
+    // (1) <-> (2) <-> (3) <-> (4) -> (5)
+    
+    T save = tail.item;
+    tail = tail.prev;
+    size -= 1;
+    return save;
+
+    // head         tail
+    // (1) -> (2) -> (3) -> (4) //-> (5)//
+  }
+  public T get(int index){
+  
+    // out of bounds < >
+    // head
+    // tail
+    if(index < 0){
+      return null;
     }
     
-    Node head;
-    Node tail;
-    int size;
-
-    public LinkedListDeque() {
-        head = null;
-        tail = null;
-        size = 0;
+    if(index > size){
+      return null;
     }
 
-    public void addFirst(T item){
-        head = new Node(item, null, null);
-        size += 1;
-    }
-
-    public void addLast(T item){
-        tail.next = new Node(item, null, tail);
-
-        // head                 tail
-        // (1) -> (2) -> (3) -> (4) -> (5)
-        
-        
-        // head                        tail
-        // (1) -> (2) -> (3) -> (4) -> (5) -> (1) ....
-        
-        tail = tail.next;
-        size += 1;
-    }
-
-    public boolean isEmpty() {
-        if(head != null){
-            return true;
-        }
-        return false;
-    }
-    
-    public int size(){
-        return size;
-    }
-    public void printDeque(){
-
-        // head                         tail
-        // (1) <-> (2) <-> (3) <-> (4) -> (5)
-        Node s = head;
-        while(s.next != null) {
-            System.out.println(s.item);
-            s = s.next;
-        }
-    }
-
-    public T removeFirst(){
-        // head          
-        // (1)
-        //  <-> (2) <-> (3) <-> (4) -> (5)
-        T save = head.item;
-        head = head.next;
-        //        head                 tail
-        //   <- (2) -> (3) -> (4) -> (5)
-        if(head != null){
-            head.prev = null;
-        }
-
-        size -= 1;
-        return save;
-    }
-    public T removeLast() {
-        // head                         tail
-        // (1) <-> (2) <-> (3) <-> (4) -> (5)
-        
-        T save = tail.item;
-        tail = tail.prev;
-        size -= 1;
-        return save;
-
-        // head                 tail
-        // (1) -> (2) -> (3) -> (4) //-> (5)//
-    }
-    public T get(int index){
-    
-        // out of bounds < >
-        // head
-        // tail
-        if(index < 0){
-            return null;
-        }
-        
-        if(index > size){
-            return null;
-        }
-
-        int counter = 0;
-        Node s = head;
-        while(s.next != null) {
-            if(counter == index){
-                return s.item;
-            }
-            s = s.next;
-            counter += 1;
-        }
-
+    int counter = 0;
+    Node s = head;
+    while(s.next != null) {
+      if(counter == index){
         return s.item;
+      }
+      s = s.next;
+      counter += 1;
     }
 
-    public T getRecursive(int index){
-        // getRecursive - establish the base case, and with your starting value get closer
-        if(index == 0){
-            return head.item;
-        } else {
-            head = head.next;
-            return getRecursive(index - 1);
-        }
-        // index 
-        //            x        index
-        //          head                         tail
-        // (1) <-> (2) <-> (3) <-> //(4) -> (5)
+    return s.item;
+  }
+
+  public T getRecursive(int index){
+    // getRecursive - establish the base case, and with your starting value get closer
+    if(index == 0){
+      return head.item;
+    } else {
+      head = head.next;
+      return getRecursive(index - 1);
     }
+    // index 
+    //      x    index
+    //      head             tail
+    // (1) <-> (2) <-> (3) <-> //(4) -> (5)
+  }
 }
 
 // LinkedListDeque = new LinkedListDeque("Bulbasaur")
--- LinkedListDequeTest.java
+++ LinkedListDequeTest.java
@@ -1,16 +1,26 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 public class LinkedListDequeTest {
-	
-	private final String firstString = "First";
+  
+  private final String firstString = "First";
 
-	@Test
-	/* Utility method for printing out empty checks. */
-	void addFirstTest() {
-		LinkedListDeque<String> test= new LinkedListDeque<String>();
-		test.addFirst("First");
-		assertEquals(firstString, test.get(0));
-	}
+  @Test
+  /* Utility method for printing out empty checks. */
+  void addFirstTest() {
+    LinkedListDeque<String> test = new LinkedListDeque<String>();
+    test.addFirst("First");
+    assertEquals(firstString, test.get(0));
+    // expected, actual
+  }
 
-	
+  @Test
+  void addFirst2Test() {
+    LinkedListDeque<String> test = new LinkedListDeque<String>();
+    test.addFirst("First");
+    test.addFirst("Second");
+    assertEquals("Second", test.get(0));
+    // expected, actual
+  }
+
+  
 } 
